55751e311b16eb9c9cd4b06504ed42bc249aa507
IT20655952 <128286993+IT20655952@users.noreply.github.com>
2023-05-23 18:57:16 +0530

    Change HomeAdmin.js

---
+++ src/Components/HomeAdmin.js
@@ -0,0 +1,90 @@
+import React from "react";
+import "../Styles/Homepage.css";
+import Slider from "./Slider";
+import NavBarHomeadmin from "./NavBar_homeAdmin";
+import Footer from "./Footer";
+import Slideshow from "./SlideShow";
+
+function Homepage() {
+	return (
+		<div className='homePage'>
+			<NavBarHomeadmin />
+			<Slideshow />
+			<div className='container'>
+				<div className='left-1'>
+				<h2>
+						Our Vison <span>Kaduwela Divisional Secretariat</span>{" "}
+					</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+						faucibus ex eget turpis feugiat, vel placerat elit eleifend.
+						Praesent hendrerit dolor sed mauris posuere tincidunt. Donec eget
+						purus tincidunt, volutpat risus ut, dictum lacus.
+					</p>
+					<button className='details-button'>more details{">>"}</button>
+				</div>
+				<div className='middle-1'>
+					<div className='image-container'>
+						<img
+							src='https://i0.wp.com/www.satipasala.org/wp-content/uploads/2020/10/Sati-Pasala-Program-at-Divisional-Secretariat-Dompe-23.jpg?ssl=1'
+							alt='placeholder'
+							className='image'
+						/>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+							faucibus ex eget turpis feugiat, vel placerat elit eleifend.
+							Praesent hendrerit dolor sed mauris posuere tincidunt. Donec eget
+							purus tincidunt, volutpat risus ut, dictum lacus.
+						</p>
+					</div>
+
+					<div className='image-container'>
+						<img
+							src='https://ciaboc.gov.lk/images/news/2023/01/0130/03/1_1.jpg'
+							alt='placeholder'
+							className='image'
+						/>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+							faucibus ex eget turpis feugiat, vel placerat elit eleifend.
+							Praesent hendrerit dolor sed mauris posuere tincidunt. Donec eget
+							purus tincidunt, volutpat risus ut, dictum lacus.
+						</p>
+					</div>
+				</div>
+				<div className='right-1'>
+					<div className='image-container'>
+						<img
+							src='https://www.cgihambantota.gov.in/webImage/1628233419_2.jpg'
+							alt='placeholder'
+							className='image'
+						/>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+							faucibus ex eget turpis feugiat, vel placerat elit eleifend.
+							Praesent hendrerit dolor sed mauris posuere tincidunt. Donec eget
+							purus tincidunt, volutpat risus ut, dictum lacus.
+						</p>
+					</div>
+
+					<div className='image-container'>
+						<img
+							src='https://pbs.twimg.com/media/E6iwaTSVoAAuk4-?format=jpg&name=large'
+							alt='placeholder'
+							className='image'
+						/>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+							faucibus ex eget turpis feugiat, vel placerat elit eleifend.
+							Praesent hendrerit dolor sed mauris posuere tincidunt. Donec eget
+							purus tincidunt, volutpat risus ut, dictum lacus.
+						</p>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
+}
+
+export default Homepage;
